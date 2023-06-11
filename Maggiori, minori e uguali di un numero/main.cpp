3549b2e22cac7f957ca45565c9d122e09366622b
Muhammad-Shaya <m.shayan@gmail.com>
2023-06-11 19:34:32 +0200

    Es 34 Pag 63

---
+++ Maggiori, minori e uguali di un numero/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k, maggiori = 0, numeri, minori = 0, uguali = 0;
+    cout << "Inserisci un valore numerico k: ";
+    cin >> k;
+    cout << "Inserisci quanti valori vuoi leggere: ";
+    cin >> n;
+    cout << "Inserire " << n << " numeri: " << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> numeri;
+        if (numeri == k)
+            uguali++;
+        else if (numeri > k)
+            maggiori++;
+        else
+            minori++;
+    }
+    cout << endl;
+    cout << "I valori uguali a " << k << " sono: " << uguali << endl;
+    cout << "I valori maggiori di " << k << " sono: " << maggiori << endl;
+    cout << "I valori minori di " << k << " sono: " << minori << endl;
+
+    return 0;
+}
