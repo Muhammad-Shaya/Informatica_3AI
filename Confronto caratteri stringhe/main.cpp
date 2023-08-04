f4c6f2b233226e53cefbd5c11f74da4b4577d038
Muhammad-Shaya <m.shayan@gmail.com>
2023-08-04 14:04:16 +0200

    Es 52 Pag 180

---
+++ Confronto caratteri stringhe/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int conta_caratteri_comuni(const string &s1, const string &s2) {
+    int conta = 0;
+
+    for (int i = 0; i < s1.size(); ++i) {
+        char c = tolower(s1.at(i));
+        int trovata = s2.find(tolower(c));
+
+        if (trovata != string::npos)
+            conta++;
+    }
+
+    return conta;
+}
+
+int main() {
+    string s1, s2;
+
+    cout << "Inserisci una parola: ";
+    cin >> s1;
+    cout << "Inserisci una seconda parola: ";
+    cin >> s2;
+    cout << endl;
+
+    int risultato = conta_caratteri_comuni(s1, s2);
+    cout << s1 << " e " << s2 << " hanno in comune " << risultato << " caratteri." << endl;
+
+    return 0;
+}
