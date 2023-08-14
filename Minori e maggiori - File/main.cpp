c5ae77efecf00cc3fa92f4d56aa06b98697470bd
Muhammad-Shaya <m.shayan@gmail.com>
2023-08-14 13:08:40 +0200

    Es 23 Pag 247

---
+++ Minori e maggiori - File/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("../numeri.txt");
+
+    if (!in) {
+        cout << "Apertura del file 'numeri.txt' fallita." << endl;
+        return 1;
+    }
+
+    int N;
+    cout << "Inserisci un valore intero N: ";
+    cin >> N;
+    cout << endl;
+
+    ofstream minori_out("../minori.txt");
+    ofstream maggiori_out("../maggiori.txt");
+
+    if (!minori_out || !maggiori_out) {
+        cout << "Apertura dei file 'minori.txt' e 'maggiori.txt' fallita." << endl;
+        return 1;
+    }
+
+    int numero;  //Questo numero mi serve per capire quanti numeri ci sono dentro il file "numeri.txt"
+    while (in >> numero) {
+        if (numero < N)
+            minori_out << numero << endl;
+        else
+            maggiori_out << numero << endl;
+    }
+
+    in.close();
+    minori_out.close();
+    maggiori_out.close();
+
+    cout << "I numeri maggiori di " << N << " sono stati inseriti nel file 'maggiori.txt'. " << endl;
+    cout << "I numeri minori di " << N << " sono stati inseriti nel file 'minori.txt'. " << endl;
+
+    return 0;
+}
